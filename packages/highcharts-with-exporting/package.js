8bac04a9db8b8c4d3666028026886192eb6dd286
Daniel Jost <torpedojost@gmail.com>
2014-02-15 02:07:57 -0500

    Removed highcharts from repo.

--- packages/highcharts-with-exporting/package.js
+++
@@ -1,9 +0,0 @@
-Package.describe({
-    summary: "Highcharts with exporting."
-});
-
-Package.on_use(function(api) {
-    api.use('jquery', 'client');
-    api.add_files('lib/highcharts.js', 'client');
-    api.add_files('lib/exporting.src.js', 'client');
-});
